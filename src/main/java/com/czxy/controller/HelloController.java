6df343053fa6d76f8c059bedcfabcdefbef51838
quanyuxiang <1733449269@qq.com>
2019-07-28 22:15:22 +0800

    本地工程承添加

---
+++ src/main/java/com/czxy/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.czxy.controller;
+
+/**
+ * @author Keximo
+ * @version v 1.0
+ * @date 2019/7/28
+ */
+public class HelloController {
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+    }
+}
